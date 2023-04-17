5f19b12922f8a3a77d4cd0b3304a42cc8b215b05
ulol <jeffk0408@naver.com>
2023-04-18 03:47:37 +0900

    feat : step1 start : 프로젝트 생성

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Ch2_ArenaBattle
+# MyArenaBattle
 Unseen Chapter2 ArenaBattle
 STEP01부터 STEP15까지 진행되며, 각 Branch에서 확인 가능합니다.
---
+++ Source/MyArenaBattle/MyArenaBattleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyArenaBattleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYARENABATTLE_API AMyArenaBattleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
